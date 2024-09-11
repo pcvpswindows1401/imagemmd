b757e06dae80951e56f3a26dbcfae0986ff4454e
pcvpswindows1401 <135259492+pcvpswindows1401@users.noreply.github.com>
2024-09-11 21:42:48 +0330

    Update main.py

--- main.py
+++ main.py
@@ -17,8 +17,10 @@ def img(m):
       "X-Prodia-Key": "4d012f3f-13f7-4a92-b1f6-ac0e65002803"
   }
 
-  response = requests.post(url, json=payload, headers=headers)
-  while True:
+  try:response = requests.post(url, json=payload, headers=headers,timeout=10)
+  except:
+    return None
+  for ij in range(30):
     try:
       time.sleep(1)
       url = "https://api.prodia.com/v1/job/"+response.json()["job"]
@@ -28,12 +30,12 @@ def img(m):
           "X-Prodia-Key": "4d012f3f-13f7-4a92-b1f6-ac0e65002803"
       }
 
-      response = requests.get(url, headers=headers)
+      response = requests.get(url, headers=headers,, timeout=10)
 
       im = response.json()["imageUrl"]
       return im
     except:pass
-      
+   return None   
 API_TOKEN = '7237066851:AAHaI3EXTlA_1ozw4Zk6THme5AAfjkHx0Co'
 bot = telebot.TeleBot(API_TOKEN)
 
@@ -44,8 +46,11 @@ def echo_message(message):
         bot.reply_to(message, "ok")
     else:
       image = img(message.text)
-      chat_id = message.chat.id
-      bot.send_photo(chat_id, image)
+      if image == None:
+        bot.reply_to(message, "None!")
+      else:  
+        chat_id = message.chat.id
+        bot.send_photo(chat_id, image)
 
 
 bot.polling()
